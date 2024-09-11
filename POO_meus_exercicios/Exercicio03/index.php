97ea4396f30317fbd97569b1434c4e90df1af8f4
Bruna Tholl <bruna.tholl.ti@gmail.com>
2024-09-10 21:49:54 -0300

    poo

--- POO_meus_exercicios/Exercicio03/index.php
+++ POO_meus_exercicios/Exercicio03/index.php
@@ -3,18 +3,18 @@
 /*Crie uma classe chamada CalculadoraCientifica, esta classe deve estender de Calculado 
 e implementar o seguinte método: Fatorial(); //Irá utilizar somente a variável valor1;
  */
-
+require_once("Calculadora.php");
 require_once("CalculadoraCientifica.php");
 
 $oCalc = new CalculadoraCientifica();
 
-$oCalc->valor1 = 10;
-$oCalc->valor2 = 15;
+// $oCalc->valor1 = 10;
+// $oCalc->valor2 = 15;
 
-$oCalc->Somar();
-$oCalc->Subtrair();
-$oCalc->Multiplicar();
-$oCalc->Dividir();
+// $oCalc->Somar();
+// $oCalc->Subtrair();
+// $oCalc->Multiplicar();
+// $oCalc->Dividir();
 
 // Fatorial
 $oCalc->valor1 = 5;
